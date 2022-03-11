480175e4cadea9989dcdc87042e31b1d6a19c527
Phelim Bradley <wave@phel.im>
2022-03-11 13:15:14 +0000

    Added additional validation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 import json
 import urllib.request
+from replit import db
 
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
@@ -11,8 +12,7 @@ from_number = os.environ['FROM_NUMBER']
 client = Client(account_sid, auth_token)
 
 # RETURN_URL = "https://frailenchantingparentheses.phelimbradley.repl.co/sms-response"
-RETURN_URL = "https://hooks.zapier.com/hooks/catch/5919068/bs3iuuf/"
-
+return_url = os.environ['RETURN_URL'] 
 
 app = Flask('app')
 
@@ -27,21 +27,25 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Start our TwiML response
     resp = MessagingResponse()
+    number=request.values['From']
+    body=request.values['Body']
+  
     try: 
-      response=int(request.values['Body'].strip())
+      response_index=int(body.strip())
   
-      return_value = {"response":request.values['Body'],"number":request.values['From']}
+      return_value = {"response":body,"number":number}
+      response_string=db[number]["responses"][response_index-1]
   
-      req = urllib.request.Request(RETURN_URL)
+      req = urllib.request.Request(return_url)
       req.add_header('Content-Type', 'application/json; charset=utf-8')
       jsondata = json.dumps(return_value)
       jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
       req.add_header('Content-Length', len(jsondataasbytes))
       post_response = urllib.request.urlopen(req, jsondataasbytes)
       # Add a message
-      resp.message("Thank you for answering our survey with: "+str(response))
+      resp.message("Thank you for answering our survey with: "+response_string)
     except:
-      resp.message("Hmm, we couldn't read your reply. Please try with a number 1-10.")
+      resp.message("Hmm, we couldn't read your reply. Please try with a number 1-"+str(len(db[number]["responses"])))
 
     return str(resp)
 
@@ -61,6 +65,7 @@ def send_sms():
   
   for number in data["numbers"]:
     print(number)
+    db[number]=data
     message = client.messages \
                 .create(
                      body=body,
